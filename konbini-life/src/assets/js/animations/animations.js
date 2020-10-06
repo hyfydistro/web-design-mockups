9f17d76bc70c82436f9337d8a3e061145460dc83
hyfyooie <hyfyooie@outlook.com>
2020-10-06 15:50:20 +0700

    Add media query to 'fade-in' event listener

--- konbini-life/src/assets/js/animations/animations.js
+++ konbini-life/src/assets/js/animations/animations.js
@@ -3,36 +3,31 @@ console.log("Initiating animation");
 const faders = document.querySelectorAll('.fade-in');
 const sliders = document.querySelectorAll('.slide-in');
 
-// Configurations: appear when the entire content is in view
+// * Configurations: "fade-in"
+// appear when the entire content is in view
+
+// DEFAULT || FOR DESKTOP-TABLET
 const appearOptions = {
     threshold: 1,
     rootMargin: "0px 0px -100px 0px"
 };
 
-// Slide in Configurations
+// FOR MOVILE
+const appearOptionsMobileView = {
+    threshold: 1,
+    rootMargin: "0px 0px 0px 0px"
+};
+
+// * Configurations: "slide-in"
+
+// DEFAULT
 const sliderOptions = {
     threshold: 0,
     rootMargin: "0px 0px -250px 0px"
 };
 
-// Faders Event Listerner
 
-const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting) {
-            return;
-        } else {
-            entry.target.classList.add('appear');
-            appearOnScroll.unobserve(entry.target);
-        }
-    });
-}, appearOptions);
-
-faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-})
-
-// Sliders Event Listener
+// # Sliders Event Listener
 
 const slideInOnScroll = new IntersectionObserver(function(entries, slideInOnScroll) {
     entries.forEach(entry => {
@@ -48,3 +43,41 @@ const slideInOnScroll = new IntersectionObserver(function(entries, slideInOnScro
 sliders.forEach(slider => {
     slideInOnScroll.observe(slider)
 })
+
+const mq = window.matchMedia("(max-width: 600px)");
+
+// # Faders Event Listerner
+
+if (mq.matches) {
+    // ! UNDER TESTING
+    console.log("media query working");
+    const appearOnScrollMobileView = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptionsMobileView);
+
+    faders.forEach(fader => {
+        appearOnScrollMobileView.observe(fader);
+    })
+} else {
+    const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, appearOptions);
+
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    })
+}
